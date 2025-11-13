5fbfabe47360c149acf2163ee27b92e172b428ee
hanum <dlgksdma15@naver.com>
2025-11-13 17:09:55 +0900

    [setting]: 초기 설정

---
+++ src/test/java/com/nhnacademy/Coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.Coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
