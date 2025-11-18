f980a81378f20a3d0e963493642dcb692e0d2735
hanum <dlgksdma15@naver.com>
2025-11-18 18:46:09 +0900

    [fix]: restcontroller

--- src/main/java/com/nhnacademy/coupon/controller/TestController.java
+++ src/main/java/com/nhnacademy/coupon/controller/TestController.java
@@ -3,8 +3,9 @@ package com.nhnacademy.coupon.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/coupons")
 public class TestController {
 
