5de55b3033c07922737589043611bdc009fc2c4d
hanum <dlgksdma15@naver.com>
2025-11-18 12:49:06 +0900

    [feat]: coupon eureka 설정

--- src/main/java/com/nhnacademy/coupon/controller/TestController.java
+++ src/main/java/com/nhnacademy/coupon/controller/TestController.java
@@ -2,8 +2,10 @@ package com.nhnacademy.coupon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/coupons")
 public class TestController {
 
     @GetMapping("/hello")
