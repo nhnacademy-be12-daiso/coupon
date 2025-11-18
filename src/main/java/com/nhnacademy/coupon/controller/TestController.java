4be74a98e73435787da5c70b71028ff076cac16e
hanum <dlgksdma15@naver.com>
2025-11-18 11:30:18 +0900

    [cicd]: cicd test

---
+++ src/main/java/com/nhnacademy/coupon/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.coupon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/hello")
+    public String test(){
+        return "test";
+    }
+}
